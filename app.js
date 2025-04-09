f16a36378e59ed3425623a5cd9f8cc6c218ca904
Timur dev <a.g.e.n.t.0.0.8tim@gmail.com>
2025-04-09 23:56:02 +0500

    third commit two

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 let numFirst = Number(prompt("Введите первое число"))
-let operator = prompt("Введите оператор для данной операции")
-let numSecond = Number(prompt("Введите второе число"))
 
-switch (operator) {
-  case "+":
-    alert(numFirst + numSecond )
-    break;
-
-  case "-":
-    alert(numFirst - numSecond)
-    break;
-
-  case "*":
-    alert(numFirst * numSecond)
-    break;
-
-  case "/":
-    alert(numFirst / numSecond)
-    break;
-
-  default:
-    alert("Вы неправильно заполнили поля, введите заново")
-    break;
+if (!isNaN(numFirst)) {
+  let operator = prompt("Введите оператор для данной операции (+ ; - ; * ; /)")  
+  let numSecond = Number(prompt("Введите второе число"))
+  if (!isNaN(numSecond)) {
+    switch (operator) {
+      case "+":
+        alert(numFirst + numSecond )
+        break;
+    
+      case "-":
+        alert(numFirst - numSecond)
+        break;
+    
+      case "*":
+        alert(numFirst * numSecond)
+        break;
+    
+      case "/":
+        alert(numFirst / numSecond)
+        break;
+    
+      default:
+        alert("Вы неправильно заполнили поля, введите заново")
+        break;
+    }
+    
+  } else {
+    alert("Вводите числа, а не буквы ")     
+  }  
+} else {
+  alert("Вводите числа, а не буквы ")
 }
+
